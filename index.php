b958cfe4f977bc0580aa65fbde4190bf4e58735c
php-academy <php-academy@mail.ru>
2014-09-16 20:36:55 +0700

    Consts

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 class Greeting {
+    const NUMBER = 1; 
     static public $name = 'Ivan';
     static public function sayHello()
     {
         echo "Hello, " . self::$name . "!";
+        //echo self::NUMBER;
     }
 }
-echo Greeting::$name;
-Greeting::sayHello();
+//echo Greeting::$name;
+//Greeting::sayHello();
+echo Greeting::NUMBER;
